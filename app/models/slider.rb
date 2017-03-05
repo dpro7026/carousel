49860d491ff17fc1ac2b60aac348aa9088e00683
Stephanie Wagner <stephwag@stephwag.com>
2017-03-04 22:44:39 -0500

    don't validate for now

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,7 +1,7 @@
 
 class Slider < ApplicationRecord
   has_attached_file :image,
-                    styles: { large: "1920x1080>" },
+                    #styles: { large: "1920x1080>" },
                     storage: :fog,
                     path: 'uploads/:filename',
                     fog_directory: 'uploads',
@@ -16,5 +16,6 @@ class Slider < ApplicationRecord
                       openstack_auth_omit_default_port: true
                     }
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  do_not_validate_attachment_file_type :image
 end
